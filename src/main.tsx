dc30eddc1e3f24e66b8e73af28034a0b4dc5815f
XavilordGamer1 <xavier.18.2018@hotmail.com>
2025-07-05 02:10:02 -0500

    implement the new payment botton of paypal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+// Opciones iniciales para el script de PayPal
+const initialOptions = {
+  clientId: "AVMvpHH1UfWFsifMFGkqdpteyk4o5wd0pkn3bhTYKCYJpyB4frTSaS1YSi1jg_1OVT4wsFC4hRn2h1NJ", // ✅ Corregido
+  currency: "USD",
+  intent: "capture",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PayPalScriptProvider options={initialOptions}>
+      <App />
+    </PayPalScriptProvider>
   </StrictMode>
 );
